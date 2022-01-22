4cf1b11ee06e5da741dc3da7c28785f0990029aa
Saya-de <sven_behrend@web.de>
2022-01-22 23:54:54 +0100

    Projektdateien hinzufügen.

---
+++ InfinityTeleport/Main.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEditor;
+using UnityEngine;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnhollowerRuntimeLib.XrefScans;
+using VRC.Animation;
+using VRC.Core;
+using VRC.SDKBase;
+
+
+namespace InfinityTeleport
+{
+    public static class BuildInfo
+    {
+        public const string Name = "InfinityTeleport";
+        public const string Description = "Teleport to Infinity"; 
+        public const string Author = "Saya"; 
+        public const string Company = null;
+        public const string Version = "1.8.7"; 
+        public const string DownloadLink = null;
+
+    }
+    public class InfinityTeleport : MelonMod
+    {
+      
+        public override void OnUpdate()
+        {
+
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.I))
+            {
+                VRCPlayer.field_Internal_Static_VRCPlayer_0.transform.position = new Vector3(9999999, 9999999, 9999999);
+            }
+
+
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.F8))
+            {               
+                R.Respawn();
+            }
+        }
+        
+    }
+    public static class R
+    {
+        private static bool XRefScanFor(this MethodBase methodBase, string searchTerm)
+        {
+            return XrefScanner.XrefScan(methodBase).Any(
+                xref => xref.Type == XrefType.Global && xref.ReadAsObject()?.ToString().IndexOf(searchTerm, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private static RespawnDelegate GetRespawnDelegate
+        {
+            get
+            {
+                if (respawnDelegate != null) return respawnDelegate;
+                MethodInfo respawnMethod = typeof(VRCPlayer).GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly).Single(m => m.GetParameters().Length == 0 && m.ReturnType == typeof(void) && m.XRefScanFor("Respawned while not in a room."));
+
+                respawnDelegate = (RespawnDelegate)Delegate.CreateDelegate(typeof(RespawnDelegate),VRCPlayer.field_Internal_Static_VRCPlayer_0,respawnMethod);
+                return respawnDelegate;
+            }
+        }
+
+        private static RespawnDelegate respawnDelegate;
+        private delegate void RespawnDelegate();
+
+        public static void Respawn()
+        {
+            GetRespawnDelegate();
+            VRCPlayer.field_Internal_Static_VRCPlayer_0.GetComponent<VRCMotionState>().Reset();
+        }
+
+    }
+}
---
+++ InfinityTeleport/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using MelonLoader;
+using InfinityTeleport;
+
+[assembly: AssemblyTitle(InfinityTeleport.BuildInfo.Description)]
+[assembly: AssemblyDescription(InfinityTeleport.BuildInfo.Description)]
+[assembly: AssemblyCompany(InfinityTeleport.BuildInfo.Company)]
+[assembly: AssemblyProduct(InfinityTeleport.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + InfinityTeleport.BuildInfo.Author)]
+[assembly: AssemblyTrademark(InfinityTeleport.BuildInfo.Company)]
+[assembly: AssemblyVersion(InfinityTeleport.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(InfinityTeleport.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(InfinityTeleport.InfinityTeleport), InfinityTeleport.BuildInfo.Name, InfinityTeleport.BuildInfo.Version, InfinityTeleport.BuildInfo.Author, InfinityTeleport.BuildInfo.DownloadLink)]
+[assembly: MelonColor(System.ConsoleColor.DarkGray)]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
